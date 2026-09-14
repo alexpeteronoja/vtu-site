2ce748726b99e80080798f76df8ca1b5594cd3d3
Alex Peter Onoja <alexpeteronoja@gmail.com>
2026-09-14 17:09:49 +0100

    production db

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import { app } from './app.js';
 
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE;
 
 mongoose
   .connect(DB)
